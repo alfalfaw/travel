8c4ee81cb39871c164144680591bc6dedb85f82f
alfalfaw <limuyle@163.com>
2020-03-21 16:22:50 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="'page-' + index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img :src="item.imgUrl" alt="" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+          desc: '火车篇'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+          desc: '度假'
+        },
+        {
+          id: '0005',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0006',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0007',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0008',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0009',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/variables';
+@import '~styles/mixin';
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+.icons {
+  .icon {
+    position: relative;
+    float: left;
+    width: 25%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 25%;
+
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+
+      box-sizing: border-box;
+      padding: 0.1rem;
+      img {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+      }
+    }
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-height: 0.44rem;
+      text-align: center;
+      color: $darkTextColor;
+      @include ellipsis;
+    }
+  }
+}
+</style>
