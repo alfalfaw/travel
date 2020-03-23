25f129fe2fd9ff586b8ae71f12ec2cedc84a40d7
alfalfaw <limuyle@163.com>
2020-03-23 11:49:43 +0800

    fix some bugs

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -44,6 +44,9 @@ export default {
   },
   mounted() {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
